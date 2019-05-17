d29251b716762514b8ad69a0e0e7b1a5723ec350
Chris Abner <chrisabner@hotmail.com>
2019-05-17 14:56:15 -0500

    1 Commit

---
+++ application/models/Usuarios_Model.php
@@ -0,0 +1,42 @@
+<?php 
+class Usuarios_Model extends CI_Model {
+
+    var $title   = '';
+    var $content = '';
+    var $date    = '';
+
+    function __construct()
+    {
+        // Call the Model constructor
+        parent::__construct();
+    }
+    
+    function get_last_ten_entries()
+    {
+        $query = $this->db->get('entries', 10);
+        return $query->result();
+    }
+    function Agregar_Usuario($Datos){
+        $this->db->create($Datos);
+    }
+
+    function insert_entry()
+    {
+        $this->title   = $_POST['title']; // please read the below note
+        $this->content = $_POST['content'];
+        $this->date    = time();
+
+        $this->db->insert('entries', $this);
+    }
+
+    function update_entry()
+    {
+        $this->title   = $_POST['title'];
+        $this->content = $_POST['content'];
+        $this->date    = time();
+
+        $this->db->update('entries', $this, array('id' => $_POST['id']));
+    }
+
+}
+?>
